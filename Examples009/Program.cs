ba5d32417d9eba2e824a823644490aa6a8f8d603
MayerBeck <mayerbeck83@gmail.com>
2022-10-27 19:02:35 +0300

    seventh commit

--- Examples009/Program.cs
+++ Examples009/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19,};
+
+int result = Max
+{
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+
+Console.WriteLine(result);
