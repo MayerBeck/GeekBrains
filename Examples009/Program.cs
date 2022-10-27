b1576b56643937c6d6618827246aab9ba08f4946
MayerBeck <mayerbeck83@gmail.com>
2022-10-27 19:08:34 +0300

    ninth commit

--- Examples009/Program.cs
+++ Examples009/Program.cs
@@ -1,20 +1,17 @@
-﻿int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19,};
-
-int result = Max
+﻿int Max(int arg1, int arg2, int arg3)   // Функция называется Max и имеет на входе три аргумента
 {
+    int result = arg1;                  // В переменную result присваиваем арг1
+    if(arg2 > result) result = arg2;    // Если арг2 больше result, то result теперь арг2
+    if(arg3 > result) result = arg3;    // Если арг3 больше result, то result теперь арг3
+    return result;                      // Вызвращаем result обратоно
+}
+
+int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19,};
+
+int result = Max(
     Max(array[0], array[1], array[2]),
     Max(array[3], array[4], array[5]),
     Max(array[6], array[7], array[8])
-}
-
-int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 23;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23;
-int c3 = 33;
+);
 
 Console.WriteLine(result);
