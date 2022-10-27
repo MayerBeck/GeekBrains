4a5fc32598fa5df7e03a8d32088ed905857fb6bc
MayerBeck <mayerbeck83@gmail.com>
2022-10-27 11:47:30 +0300

    first commit

---
+++ Examples000/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+
+dotnet new console  -   Создать новый проект
+dotnet run  -   Запустить проект
+
+Console.Write и Console.WriteLine   -   Вывода информации в консоль
+
+
+
+
+*/
+
+Console.WriteLine("Введите имя пользователя");
+string username1 = Console.ReadLine();
+Console.WriteLine("Введите фамилию пользователя");
+string username2 = Console.ReadLine();
+Console.WriteLine("Введите отчество пользователя");
+string username3 = Console.ReadLine();
+Console.WriteLine($"Приветствую тебя, {username2} {username1} {username3}");
