38a68b90c65fdbfb2078fb70365361e22ece627e
MayerBeck <mayerbeck83@gmail.com>
2022-10-27 11:58:28 +0300

    second commit

--- Examples000/Program.cs
+++ Examples000/Program.cs
@@ -5,15 +5,6 @@ dotnet run  -   Запустить проект
 
 Console.Write и Console.WriteLine   -   Вывода информации в консоль
 
-
-
-
 */
-
-Console.WriteLine("Введите имя пользователя");
-string username1 = Console.ReadLine();
-Console.WriteLine("Введите фамилию пользователя");
-string username2 = Console.ReadLine();
-Console.WriteLine("Введите отчество пользователя");
-string username3 = Console.ReadLine();
-Console.WriteLine($"Приветствую тебя, {username2} {username1} {username3}");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
